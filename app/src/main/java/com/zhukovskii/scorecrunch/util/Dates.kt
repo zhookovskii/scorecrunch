8fa234b80b30ede5a88b8fa7676883d65325ebd2
zhookovskii <98118965+zhookovskii@users.noreply.github.com>
2024-07-11 22:05:48 +0300

    remove unused date functions

--- app/src/main/java/com/zhukovskii/scorecrunch/util/Dates.kt
+++ app/src/main/java/com/zhukovskii/scorecrunch/util/Dates.kt
@@ -9,9 +9,6 @@ private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
 private fun parseDate(dateString: String) = LocalDate.parse(dateString, formatter)
 
-fun parseDateISO(dateString: String): LocalDate? =
-    LocalDate.parse(dateString, DateTimeFormatter.ISO_DATE_TIME)
-
 fun String.isBefore(): Boolean {
     val inputDate = parseDate(this)
     return inputDate.isBefore(LocalDate.now())
@@ -20,16 +17,6 @@ fun String.isBefore(): Boolean {
 fun String.isAfter(): Boolean {
     val inputDate = parseDate(this)
     return inputDate.isAfter(LocalDate.now())
-}
-
-fun afterDays(daysNumber: Int): String {
-    val newDate = LocalDate.now().plusDays(daysNumber.toLong())
-    return newDate.format(formatter)
-}
-
-fun beforeDays(daysNumber: Int): String {
-    val newDate = LocalDate.now().minusDays(daysNumber.toLong())
-    return newDate.format(formatter)
 }
 
 fun String.formatDate(): String {
