c78c6b3be12294b2912d340ef26e1f4e250908b1
ZHukovskii_Nikita <ssbakungoff@gmail.com>
2024-07-11 07:22:17 +0300

    remove invalidated api key

--- app/src/main/java/com/zhukovskii/scorecrunch/config/Config.kt
+++ app/src/main/java/com/zhukovskii/scorecrunch/config/Config.kt
@@ -1,6 +1,6 @@
 package com.zhukovskii.scorecrunch.config
 
 object Config {
-    const val API_KEY = "e76af641d08e40eab34f04a83c655231"
+    const val API_KEY = ""
     const val BASE_URL = "https://api.football-data.org/"
 }
